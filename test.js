5adc884360f1d9c06d031d6ce351aae93b06b3bf
CJ Patoilo <cjpatoilo@gmail.com>
2018-01-31 02:31:00 -0300

    Define test with format result expected

--- test.js
+++ test.js
@@ -1,6 +1,26 @@
 const test = require('ava')
 const pkg = require('./package.json')
+const pkgLock = require('./package-lock.json')
+const rasper = require('./')
+
+test('main feature', t => {
+	const argv = rasper(
+		'pwa-installer dist/**/*.html -f 3 -n -o 4 foo bar baz --manifest --config=pwa.config.js 2 4 -f=99'
+			.split(' ')
+			.slice(1)
+	)
+	const expect = {
+		_: [ 'dist/**/*.html', 'foo', 'bar', 'baz', 2, 4 ],
+		n: true,
+		o: 4,
+		manifest: true,
+		config: 'pwa.config.js',
+		f: 99
+	}
+
+	t.deepEqual(argv, expect)
+})
 
 test('version', t => {
-	t.is(pkg.version, pkg.version)
+	t.is(pkg.version, pkgLock.version)
 })
